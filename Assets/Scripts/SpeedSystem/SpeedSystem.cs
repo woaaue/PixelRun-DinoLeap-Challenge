cd1b6c4313b55b32f19fb7daf4632fe36e0eee13
woaaue <124384635+woaaue@users.noreply.github.com>
2024-02-01 19:14:09 +0300

    editing speed system

--- Assets/Scripts/SpeedSystem/SpeedSystem.cs
+++ Assets/Scripts/SpeedSystem/SpeedSystem.cs
@@ -3,15 +3,20 @@ using UnityEngine;
 public sealed class SpeedSystem : MonoBehaviour
 {
     [SerializeField] private float _minSpeed;
+    [SerializeField] private float _maxSpeed;
     [SerializeField] private float _accelerationRate;
+    [SerializeField] private float _testSpeed;
 
     public static SpeedSystem instance { get; private set; }
-
     public float currentSpeed { get; private set; }
     
+    private float _speed;
+
     private void IncreaseSpeedOverTime()
     {
-        currentSpeed += _accelerationRate * Time.deltaTime;
+        _speed += _accelerationRate * Time.deltaTime;
+
+        currentSpeed = Mathf.Clamp(_speed, _minSpeed, _maxSpeed);
     }
 
     private void Awake()
@@ -27,11 +32,13 @@ public sealed class SpeedSystem : MonoBehaviour
 
     private void Start()
     {
-        currentSpeed = _minSpeed;
+        _speed = _minSpeed;
     }
 
     private void Update()
     {
         IncreaseSpeedOverTime();
+
+        _testSpeed = currentSpeed;
     }
 }
